228d7113aa3681d0314314322b676c411e804fd1
Brian Buddhadasa <brianbuddhadasa@gmail.com>
2023-01-31 22:40:55 -0500

    render info for each meal

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+import { menuArray } from "./data.js";
 
+function getFeedHtml() {
+  let feedHtml = "";
+
+  menuArray.forEach(function (menu) {
+    feedHtml += `<div>
+        <div>
+            ${menu.emoji}
+        </div>
+        <div>
+            <h2>${menu.name}<h2>
+            <p>${menu.ingredients}</p>
+            <p>$${menu.price}</p>
+        </div>
+        <div>
+            <button>+</button>
+        </div>
+    </div>`;
+  });
+
+  return feedHtml;
+}
+
+function render() {
+  document.getElementById("feed").innerHTML = getFeedHtml();
+}
+
+render();
